6b02b1799e7507af3c90c4aacb5acfc10f79813c
Decilap denis <decilapdenis@gmail.com>
2021-01-27 22:57:24 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function(){
+    var list = document.querySelectorAll(".card h2");
+    var i=0;
+    while (i < list.length) {
+      acc[i].addEventListener("click", function () {
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+      i++;
+    }
+
+    document.querySelector('.random button').onclick=function(e){
+      var index = Math.floor(Math.random() * 19);
+      e.target.nextElementSibling.textContent = array[index];
+    }
+})();
