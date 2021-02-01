5e4d842f6a7c28c300b173ad8e3d75859f39e9cf
Decilap denis <decilapdenis@gmail.com>
2021-02-01 23:19:21 +0100

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,42 +1,53 @@
 (function(){
-    var array = [
-        'Farima Doumbia',
-        'Adil Rmili',
-        'Alexis Van Neyenhoff',
-        'Alicia Derradji',
-        'Asad Tahir',
-        'Bamba gaye',
-        'Caroline F',
-        'Charles Chrismann',
-        'Dasilve David',
-        'Decilap Denis',
-        'DOUCARA Lafferrière',
-        'Elesse',
-        'Flora Lafferrière',
-        'Gaëlle COUE',
-        'Jules Grand',
-        'Le Pollotec Jonathan',
-        'Mohamed Abdeloifi',
-        'Abdoulaye',
-        'Adoulaye'
-      ];
-    var list = document.querySelectorAll(".card h2");
-    var i=0;
-    while (i < list.length) {
-      
-      list[i].addEventListener("click", function () {
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-      i++;
+   //données apprenants
+   var array = [
+    'Farima Doumbia',
+    'Adil Rmili',
+    'Alexis Van Neyenhoff',
+    'Alicia Derradji',
+    'Asad Tahir',
+    'Bamba gaye',
+    'Caroline F',
+    'Charles Chrismann',
+    'Dasilve David',
+    'Decilap Denis',
+    'DOUCARA Lafferrière',
+    'Elesse',
+    'Flora Lafferrière',
+    'Gaëlle COUE',
+    'Jules Grand',
+    'Le Pollotec Jonathan',
+    'Mohamed Abdeloifi',
+    'Abdoulaye',
+    'Adoulaye'
+    ];
+  
+
+    //carrousel
+    function carrousel(elem){
+      var list = document.querySelectorAll(elem);
+      var i=0;
+      while (i < list.length) {
+        list[i].addEventListener("click", function () {
+          var panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "block";
+          }
+        });
+        i++;
+      }
     }
 
-    document.querySelector('.random button').onclick=function(e){
-      var index = Math.floor(Math.random() * 19);
-      e.target.nextElementSibling.textContent = array[index];
+    //retourne un apprenant
+    function random(array, target){
+      document.querySelector(target).onclick = function(e){
+        var index = Math.floor(Math.random() * 19);
+        e.target.nextElementSibling.textContent = array[index];
+      }
     }
+
+    carrousel('.card h2');
+    random(array, '.random button');
 })();
